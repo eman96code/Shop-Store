cafe82d49f70696d4a99e52faa4d3f0e977ccca5
eman96code <eman96abdelmawla@gmail.com>
2023-10-21 10:12:40 +0300

    adding NavBar Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <h1 className="logo">Shope Store</h1>
+      <ul className="nav">
+        <li>
+          <a href="">home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Counter</a>
+        </li>
+        <li>
+          <a href="">Shop</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default NavBar;
